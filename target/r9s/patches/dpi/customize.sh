c3ad74ffe86c420ebf74e704c3edd30b04be19d0
ditternation <ditternation@outlook.com>
2026-04-28 16:21:49 +0200

    target: fix r9s init density prop

--- target/r9s/patches/dpi/customize.sh
+++ target/r9s/patches/dpi/customize.sh
@@ -1,1 +1,6 @@
-SET_PROP "vendor" "ro.sf.init.lcd.density" "$(GET_PROP "vendor" "ro.sf.lcd.density")"
+LCD_DENSITY="$(GET_PROP "vendor" "ro.sf.lcd_density")"
+if [ "$LCD_DENSITY" ]; then
+    SET_PROP "vendor" "ro.sf.init.lcd_density" "$LCD_DENSITY"
+else
+    ABORT "ro.sf.lcd_density prop not found in vendor"
+fi
