9bb26d75663e3771861f59ab631135f0d23f79e6
ditternation <ditternation@outlook.com>
2026-04-28 10:08:09 +0200

    paradigm: refresh feature assets and patches

---
+++ unica/mods/paradigm/customize.sh
@@ -0,0 +1,56 @@
+if [ ! "$(GET_PROP "system" "ro.unica.codename")" ]; then
+    # Match latest Samsung's flagship device codename
+    ROM_CODENAME="$(basename "$MODPATH")"
+    SET_PROP "system" "ro.unica.codename" "${ROM_CODENAME^}"
+    unset ROM_CODENAME
+fi
+
+# 2025 Audio Pack
+LOG_STEP_IN "- Adding 2025 Audio Pack"
+if $TARGET_AUDIO_SUPPORT_ACH_RINGTONE; then
+    SET_PROP "vendor" "ro.config.ringtone" "ACH_Galaxy_Bells.ogg"
+    SET_PROP "vendor" "ro.config.notification_sound" "ACH_Brightline.ogg"
+    SET_PROP "vendor" "ro.config.alarm_alert" "ACH_Morning_Xylophone.ogg"
+    SET_PROP "vendor" "ro.config.media_sound" "Media_preview_Over_the_horizon.ogg"
+    SET_PROP "vendor" "ro.config.ringtone_2" "ACH_Atomic_Bell.ogg"
+    SET_PROP "vendor" "ro.config.notification_sound_2" "ACH_Three_Star.ogg"
+else
+    SET_PROP "vendor" "ro.config.ringtone" "Galaxy_Bells.ogg"
+    SET_PROP "vendor" "ro.config.notification_sound" "Brightline.ogg"
+    SET_PROP "vendor" "ro.config.alarm_alert" "Morning_Xylophone.ogg"
+    SET_PROP "vendor" "ro.config.media_sound" "Media_preview_Over_the_horizon.ogg"
+    SET_PROP "vendor" "ro.config.ringtone_2" "Atomic_Bell.ogg"
+    SET_PROP "vendor" "ro.config.notification_sound_2" "Three_Star.ogg"
+fi
+LOG_STEP_OUT
+
+# Adaptive colour tone is already included in One UI 8.5.
+
+# Media Context Analyzer
+LOG_STEP_IN "- Adding Media Context Analyzer feature"
+EVAL "ln -s \"human-pet-pose_SR-V200.tflite\" \"$WORK_DIR/system/system/etc/mediacontextanalyzer/Pose.tflite\""
+SET_METADATA "system" "system/etc/mediacontextanalyzer/Pose.tflite" 0 0 644 "u:object_r:system_file:s0"
+SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_MMFW_CONFIG_MEDIA_CONTEXT_ANALYZER_CORE" "GPU"
+SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_MMFW_SUPPORT_MEDIA_CONTEXT_ANALYZER" "TRUE"
+LOG_STEP_OUT
+
+# Audio eraser
+# Requires SEC_PRODUCT_FEATURE_MMFW_SUPPORT_MEDIA_CONTEXT_ANALYZER
+LOG_STEP_IN "- Adding Audio eraser feature"
+SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_AUDIO_CONFIG_MULTISOURCE_SEPARATOR" "{FastScanning_6, SourceSeparator_4, Version_1.3.0}"
+LOG_STEP_OUT
+
+# Now brief
+# Requires SEC_FLOATING_FEATURE_COMMON_CONFIG_AI_VERSION >= 20251
+# or SEC_FLOATING_FEATURE_FRAMEWORK_SUPPORT_AI_BRIEF_FOR_UT
+LOG_STEP_IN "- Adding Now brief feature"
+SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FRAMEWORK_SUPPORT_PERSONALIZED_DATA_CORE" "TRUE"
+LOG_STEP_OUT
+
+# Semantic search
+# Requires SEC_FLOATING_FEATURE_COMMON_CONFIG_AI_VERSION >= 20251
+DECODE_APK "system" "system/priv-app/SecSettingsIntelligence/SecSettingsIntelligence.apk"
+LOG "- Enabling Semantic search feature in /system/system/priv-app/SecSettingsIntelligence/SecSettingsIntelligence.apk"
+EVAL "cp -a \"$MODPATH/semanticsearch/SecSettingsIntelligence.apk/res/raw/\"* \"$APKTOOL_DIR/system/priv-app/SecSettingsIntelligence/SecSettingsIntelligence.apk/res/raw\""
+SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_MSCH_SUPPORT_NLSEARCH" "TRUE"
+LOG_STEP_OUT
