23a37a510d6a26c4e1aff6e642b378f432292293
ditternation <ditternation@outlook.com>
2026-04-29 21:11:19 +0200

    unica: fix Artisan updater install flags

--- unica/mods/updater/customize.sh
+++ unica/mods/updater/customize.sh
@@ -1,12 +1,14 @@
 SKIPUNZIP=1
 
 # Set ArtisanROM updater flags
-SET_PROP "system" "ro.artisanrom.version" "$UPDATER_VERSION"
+ARTISAN_UPDATER_VERSION="${UPDATER_VERSION:-$ROM_VERSION}"
+SET_PROP "system" "ro.artisanrom.version" "$ARTISAN_UPDATER_VERSION"
 SET_PROP "system" "ro.artisanrom.target" "$TARGET_CODENAME"
 
 if ! $ROM_IS_OFFICIAL; then
-    LOG "Build is not official. Skipping"
-    return 0
+    LOG "- Build is not official. Installing updater anyway"
 fi
 
 ADD_TO_WORK_DIR "$MODPATH" "system" "."
+
+unset ARTISAN_UPDATER_VERSION
