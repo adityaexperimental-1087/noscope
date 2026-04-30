5413cb7695fc85a61a3e22dcf37df82f84c931d8
Android-Artisan <romartisan2025@gmail.com>
2026-04-30 16:48:48 +0400

    Delete unica/mods/updater/customize.sh

--- unica/mods/updater/customize.sh
+++
@@ -1,4 +0,0 @@
-SET_PROP "system" "ro.artisanrom.version" "$UPDATER_VERSION"
-SET_PROP "system" "ro.artisanrom.target" "$TARGET_CODENAME"
-
-ADD_TO_WORK_DIR "$MODPATH" "system" "." 0 0 755 "u:object_r:system_file:s0"
