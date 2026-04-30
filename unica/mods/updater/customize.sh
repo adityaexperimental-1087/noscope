4317f21f361dca9e296c8933fdd76f921ea132bf
Android-Artisan <romartisan2025@gmail.com>
2026-04-30 10:54:57 +0200

    unica: mods: updater: fully fixup

--- unica/mods/updater/customize.sh
+++ unica/mods/updater/customize.sh
@@ -2,5 +2,3 @@ SET_PROP "system" "ro.artisanrom.version" "$UPDATER_VERSION"
 SET_PROP "system" "ro.artisanrom.target" "$TARGET_CODENAME"
 
 ADD_TO_WORK_DIR "$MODPATH" "system" "." 0 0 755 "u:object_r:system_file:s0"
-
-DECODE_APK "system" "system/priv-app/ArtisanUpdater/ArtisanUpdater.apk"
