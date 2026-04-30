6b66521e3ddb79bd35c7fb7c5d92a303b617659e
Android-Artisan <romartisan2025@gmail.com>
2026-04-30 10:36:53 +0200

    unica: mods: fixup again

--- unica/mods/bootlogo/customize.sh
+++
@@ -1,7 +0,0 @@
-if [[ $TARGET_OS_SINGLE_SYSTEM_IMAGE == "essi" ]]; then
-    LOG_STEP_IN "- Exynos device detected. Adding custom up_param."
-    cp -a "$SRC_DIR/unica/mods/bootlogo/up_param_1440p.bin" "$WORK_DIR/up_param.bin"
-    LOG_STEP_OUT
-else
-    LOG "- Non-Exynos device detected. Skipping custom up_param."
-fi
